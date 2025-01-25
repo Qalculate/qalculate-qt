0ed1a05a64e61359ae9e61ec75713c85eac5bc21
Hanna K. <hanna.knutsson@protonmail.com>
2025-01-25 07:58:11 +0100

    Add English translation file to compiled resources; Add plural forms to English translation file

--- translations/qalculate-qt_en.ts
+++ translations/qalculate-qt_en.ts
@@ -9,4 +9,55 @@
     See https://bugreports.qt.io/browse/QTBUG-69196
 -->
 <TS version="2.1" language="en">
+<context>
+    <name>HistoryView</name>
+    <message numerus="yes">
+        <location filename="../src/historyview.cpp" line="680"/>
+        <source>Exchange rate source(s):</source>
+        <translation>
+            <numerusform>Exchange rate source:</numerusform>
+            <numerusform>Exchange rate sources:</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message numerus="yes">
+        <location filename="../src/preferencesdialog.cpp" line="397"/>
+        <location filename="../src/preferencesdialog.cpp" line="621"/>
+        <source>%n day(s)</source>
+        <translation>
+            <numerusform>%n day</numerusform>
+            <numerusform>%n days</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>QalculateQtSettings</name>
+    <message numerus="yes">
+        <location filename="../src/qalculateqtsettings.cpp" line="1951"/>
+        <source>It has been %n day(s) since the exchange rates last were updated.
+
+Do you wish to update the exchange rates now?</source>
+        <translation>
+            <numerusform>It has been %n day since the exchange rates last were updated.
+
+Do you wish to update the exchange rates now?</numerusform>
+            <numerusform>It has been %n days since the exchange rates last were updated.
+
+Do you wish to update the exchange rates now?</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>QalculateWindow</name>
+    <message numerus="yes">
+        <location filename="../src/qalculatewindow.cpp" line="484"/>
+        <source>Open Settings Folder(s)</source>
+        <translation>
+            <numerusform>Open Settings Folder</numerusform>
+            <numerusform>Open Settings Folders</numerusform>
+        </translation>
+    </message>
+</context>
 </TS>
